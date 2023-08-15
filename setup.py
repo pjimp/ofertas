4e6d691922a325c7871cb5df469e55b514726df0
Pedro Jiménez Pizarro <pedro.jimenez.pizarro@ibm.com>
2023-08-15 15:44:03 +0200

    Init setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ofertas',
+    version='0.1',
+    description='Programa para obtener ofertas de videojuegos',
+    author='Naras',
+    author_email='naraspjp@outlook.com',
+    url='https://github.com/naraspjp/ofertas',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+    ],
+)
